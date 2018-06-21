2cc0f9ce2c89f59cbc7030168899a36b88146a42
CristianG <tapcrt@gmail.com>
2018-06-21 06:42:48 -0500

    migracion 1 con scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_06_21_104512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "edad"
+    t.string "nombre"
+    t.text "comentario"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
